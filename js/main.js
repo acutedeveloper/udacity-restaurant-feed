208ad828bc353b2ee730b34e675d41bc608c7d82
Acute Developer <nutyga@gmail.com>
2018-11-21 19:52:03 +0000

    Rsponsive Design: Adjusting elements so that they fit across various viewport widths

--- js/main.js
+++ js/main.js
@@ -157,28 +157,35 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  const div = document.createElement('div');
+
+  li.classList.add('restaurant__listing');
+  div.classList.add('restaurant__listing-content');
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  div.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  div.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  div.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  div.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.classList.add('restaurant__listing-link');
+  div.append(more);
+
+  li.append(div);
 
   return li
 }
